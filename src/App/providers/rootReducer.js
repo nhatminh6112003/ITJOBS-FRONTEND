8362883664c057ef0e2be4b6ee6ac27510fdaf6b
dat <MY_NAME@example.com>
2023-10-10 22:08:56 +0700

    implement resume_language feature

--- src/App/providers/rootReducer.js
+++ src/App/providers/rootReducer.js
@@ -16,11 +16,12 @@ import resumeObjectiveApi from './apis/resumeObjectiveApi';
 import resumeActivityApi from './apis/resumeAcitivity';
 import resumeExperienceApi from './apis/resumeExperienceApi';
 import resumeAddioninfoApi from './apis/resumeAddioninfo';
+import resumeSkillApi from './apis/resumeSkill';
 import workTypeApi from './apis/workTypeApi';
 import resumeDesiredJobApi from './apis/resumeDesiredJobApi';
 import listProvincesApi from './apis/listProvincesApi';
 import districtsApi from './apis/districtsApi';
-
+import resumeLanguageApi from './apis/resumeLanguageApi';
 const rootReducer = combineReducers({
 	[authSlice.name]: authSlice.reducer,
 	[themeSlice.name]: themeSlice.reducer,
@@ -39,7 +40,13 @@ const rootReducer = combineReducers({
 	[resumeActivityApi.reducerPath]: resumeActivityApi.reducer,
 	[resumeExperienceApi.reducerPath]: resumeExperienceApi.reducer,
 	[resumeAddioninfoApi.reducerPath]: resumeAddioninfoApi.reducer,
-	[workTypeApi.reducerPath]: workTypeApi.reducer
+	[workTypeApi.reducerPath]: workTypeApi.reducer,
+	[resumeSkillApi.reducerPath]: resumeSkillApi.reducer,
+	[resumeDesiredJobApi.reducerPath]: resumeDesiredJobApi.reducer,
+	[workTypeApi.reducerPath]: workTypeApi.reducer,
+	[listProvincesApi.reducerPath]: listProvincesApi.reducer,
+	[districtsApi.reducerPath]: districtsApi.reducer,
+	[resumeLanguageApi.reducerPath]: resumeLanguageApi.reducer,
 });
 
 export default rootReducer;
