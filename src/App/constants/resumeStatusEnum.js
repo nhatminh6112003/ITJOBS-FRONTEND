fbee92ff2603cf004027993dd3c40252331ea615
dat <MY_NAME@example.com>
2023-12-20 00:06:30 +0700

    fix: resume status enum

--- src/App/constants/resumeStatusEnum.js
+++ src/App/constants/resumeStatusEnum.js
@@ -1,10 +1,7 @@
 export const ResumeStatusEnum = {
 	UNDECIDED: 0, // Chưa quyết định
 	NOT_MATCHED: 1, // Không phù hợp
-	REJECTED: 2, // Từ chối
-	UNDER_REVIEW: 3, // Kiểm tra
 	INTERVIEW: 4, // Phỏng vấn
-	OFFERED: 5, // Đề nghị tuyển dụng
 	HIRED: 6 // Nhận việc
 };
 export const ResumeStatusOptions = [
